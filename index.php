d4b7623352bd7099c8dcc40524be14a00a8be82b
chema <jose.rubio12@educa.madrid.org>
2023-09-28 20:27:27 +0200

    Casi terminada la tabla horarios

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-aqui va el html
+<?php
+    include('data.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ConDieguin</title>
+</head>
+<body>
+    <!-- Lunes -->
+    <div class="DIW" id="DIWL"></div>
+    <div class="DWES" id="DWESL1"></div>
+    <div class="DWES" id="DWESL2"></div>
+    <div class="ITGS" id="ITGSL"></div>
+
+    <!-- Martes -->
+    <div class="DWEC" id="DWECM"></div>
+    <div class="EIE" id="EIEM1"></div>
+    <div class="DWES" id="DWESM"></div>
+
+    <!-- Miercoles -->
+    <div class="DIW" id="DIWX"></div>
+    <div class="EIE" id="EIEX1"></div>
+    <div class="EIE" id="EIEX2"></div>
+    <div class="DAW" id="DAWX"></div>
+
+    <!-- Jueves -->
+    <div class="DWEC" id="DWECJ"></div>
+    <div class="DAW" id="DAWJ1"></div>
+
+
+</body>
+</html>
