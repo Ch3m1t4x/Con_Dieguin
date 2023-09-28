1dd9d9478b6f2aae855a2700adac95834602f73a
Diego <diego.fernandezleanos@educa.madrid.org>
2023-09-28 20:30:15 +0200

    añadir array de horario

--- data.php
+++ data.php
@@ -1,1 +1,11 @@
-aqui va todo el php
+<?php 
+$modulos = [
+    ["nombre" => "DWES", "profesor" => "IF08", "descripcion" => "Módulo con un profe increible"],
+    ["nombre" => "DAW", "profesor" => "IF08", "descripcion" => "Módulo con un profe increible"],
+    ["nombre" => "DIW", "profesor" => "SIF06", "descripcion" => "Módulo que trata el desarrollo de la interfaz web"],
+    ["nombre" => "DWEC", "profesor" => "SIF06", "descripcion" => "Módulo que trata la funcionalidad del lado cliente"],
+    ["nombre" => "EIE_DAW", "profesor" => "FOL2", "descripcion" => "Módulo que trata el mundo emprendedor"],
+    ["nombre" => "ITGS", "profesor" => "I13", "descripcion" => "Módulo en el que se estudia ingles técnico"],
+    ["nombre" => "recreo", "profesor" => "", "descripcion" => ""]
+];
+?>
